b026e4e7ec270e2c64abcd8c42709fc7a3702dea
BGuerrero825 <bguerrero825@gmail.com>
2022-08-19 07:00:57 -0500

    crappy platformer physics

--- Player.gd
+++ Player.gd
@@ -18,13 +18,17 @@ func _process(delta):
 	# process user x-axis velocity input based on move keys
 	input_dir = Vector2.ZERO
 	input_dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	print(input_dir.x)
+	if input_dir.x == 1: $Sprite.flip_h = false
+	elif input_dir.x == -1: $Sprite.flip_h = true
 	# calculate speed based on input vector and ACCEL and FRICTION constants
 	if input_dir.x == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, SPEED * input_dir.x, ACCEL * delta)
-	# John Mayer
+	# jump
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -60
 	velocity.y = move_toward(velocity.y, GRAVITY, GRAVITY * delta)
+	print(velocity)
 	velocity = move_and_slide(velocity)
 	
