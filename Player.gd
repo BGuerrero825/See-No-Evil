643c8014cc9263ee05b9ddd5311e867cc1dc07a0
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2022-08-19 23:47:43 -0500

    jump fixed

--- Player.gd
+++ Player.gd
@@ -21,15 +21,18 @@ func _process(delta):
 	input_dir.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	if input_dir.x == 1: $Sprite.flip_h = false
 	elif input_dir.x == -1: $Sprite.flip_h = true
+	
 	# calculate velocity based on input vector, ACCEL and FRICTION constants
 	if input_dir.x == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	else:
 		velocity.x = move_toward(velocity.x, SPEED * input_dir.x, ACCEL * delta)
-	# jump stuff, currently works in air (no grounded check)
-	if Input.is_action_just_pressed("jump"):
+	
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -JUMP_STRENGTH
+	
 	velocity.y = move_toward(velocity.y, GRAVITY*10, GRAVITY * delta)
 	# move
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
