0c2f23b92f16e9d849b5b9832f35b6cf1a146a2d
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2022-08-20 00:22:50 -0500

    started ghost

---
+++ Ghost.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+
+# dist / sec
+const SPEED = 2000
+# speed / sec
+const ACCEL = 600
+const FRICTION = 400
+const ROTATION_SPEED = 0.3
+
+var velocity := Vector2(0,0)
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+func rotate_towards(target_pos, target_rotation_speed = ROTATION_SPEED) -> float:
+	var target_angle = PI + position.angle_to_point(target_pos)
+	$center.rotation = lerp_angle($center.rotation, target_angle, target_rotation_speed)
+
+	return $center.rotation + TAU/4
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+#	velocity.x = move_toward(velocity.x, SPEED, ACCEL * delta)
+#	rotate_towards(Vector2.ZERO)
+#	rotate_towards(Vector2(1, 1))
+#	print(rotation)
+	
+	velocity = velocity.move_toward(Vector2.UP, SPEED)
+	
+	
+	velocity = move_and_slide(velocity)
